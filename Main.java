4cd8b51863e7ba9aed57c8d33dd53a649e9f355d
sharadgulatigloballogic <115814346+sharadgulatigloballogic@users.noreply.github.com>
2025-10-15 23:48:05 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,26 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        // Fixed approach: Use a numeric string and parse it
-        Object obj = "123"; // String containing a numeric value
-        
-        // Proper way to convert String to Integer
-        Integer num = Integer.parseInt((String) obj);
+        Object obj = "Hello World"; // obj actually holds a String
+
+        Integer num = (Integer) obj; // ❌ invalid cast
         System.out.println(num);
-    }
-    
-    /**
-     * Converts an Object containing a String to Integer
-     * @param obj Object containing a String with numeric value
-     * @return Integer value
-     * @throws NumberFormatException if the String doesn't contain a parsable integer
-     */
-    public static Integer convertToInteger(Object obj) {
-        if (obj instanceof String) {
-            return Integer.parseInt((String) obj);
-        } else if (obj instanceof Integer) {
-            return (Integer) obj;
-        } else {
-            throw new IllegalArgumentException("Object must be String or Integer");
-        }
     }
 }
